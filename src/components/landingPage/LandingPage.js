54eac69a4bacdfbd129a9ae7f3af256621016a2c
mmaesom <winifred.esomonu@stetis.com>
2022-02-09 15:23:57 +0100

    header, header image and content

--- src/components/landingPage/LandingPage.js
+++ src/components/landingPage/LandingPage.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import "./landingpage.css";
 
+import Portfolio from "../portfolio/Portfolio";
+
 import {
   Box,
   Container,
@@ -13,34 +15,43 @@ import {
   Button,
   Tooltip,
   MenuItem,
+  Grid,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
+import InfoIcon from "@mui/icons-material/Info";
+import WorkIcon from "@mui/icons-material/Work";
+import ArticleIcon from "@mui/icons-material/Article";
+import PhoneIcon from "@mui/icons-material/Phone";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
 
-const pages = ["About", "Portfolio", "Resume", "Contact"];
+import imgFive from "../../assets/images/imgFive.jpg";
+import blobThree from "../../assets/images/blobThree.svg";
+import resume from "../../assets/files/resume.pdf";
+
+// const pages = ["About", "Portfolio", "Resume", "Contact"];
+const pages = [
+  { name: "About", icon: <InfoIcon /> },
+  { name: "Portfolio", icon: <WorkIcon /> },
+  { name: "Resume", icon: <ArticleIcon /> },
+  { name: "Contact", icon: <PhoneIcon /> },
+];
 const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
 const LandingPage = () => {
   const [anchorElNav, setAnchorElNav] = useState(null);
-  const [anchorElUser, setAnchorElUser] = useState(null);
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
   };
 
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
   };
 
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-
   return (
     <div>
-      {/* <CssBaseline /> */}
       <Container className="main-container">
         <Box className="inner-container">
           <AppBar position="static" className="appbar">
@@ -49,19 +60,21 @@ const LandingPage = () => {
                 <Typography
                   variant="h6"
                   noWrap
+                  className="logo"
                   component="div"
-                  sx={{ mr: 2, display: { xs: "none", md: "flex" } }}>
-                  LOGO
+                  sx={{ mr: 0, display: { xs: "none", md: "flex" } }}>
+                  WE
                 </Typography>
 
-                <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+                <Box
+                  sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
+                  className="mobile-menu">
                   <IconButton
                     size="large"
                     aria-label="account of current user"
                     aria-controls="menu-appbar"
                     aria-haspopup="true"
-                    onClick={handleOpenNavMenu}
-                    color="inherit">
+                    onClick={handleOpenNavMenu}>
                     <MenuIcon />
                   </IconButton>
                   <Menu
@@ -82,8 +95,8 @@ const LandingPage = () => {
                       display: { xs: "block", md: "none" },
                     }}>
                     {pages.map((page) => (
-                      <MenuItem key={page} onClick={handleCloseNavMenu}>
-                        <Typography textAlign="center">{page}</Typography>
+                      <MenuItem key={page.name} onClick={handleCloseNavMenu}>
+                        <Typography textAlign="center">{page.name}</Typography>
                       </MenuItem>
                     ))}
                   </Menu>
@@ -91,56 +104,133 @@ const LandingPage = () => {
                 <Typography
                   variant="h6"
                   noWrap
+                  className="logo"
                   component="div"
                   sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-                  LOGO
+                  WE
                 </Typography>
-                <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+                <Box
+                  sx={{
+                    flexGrow: 1,
+                    display: { xs: "none", md: "flex" },
+                    justifyContent: "center",
+                  }}>
                   {pages.map((page) => (
                     <Button
-                      key={page}
+                      className="appbar-links"
+                      key={page.name}
                       onClick={handleCloseNavMenu}
                       sx={{ my: 2, color: "white", display: "block" }}>
-                      {page}
+                      <div>
+                        <div>{page.icon}</div>
+                      </div>
+                      {page.name}
                     </Button>
                   ))}
                 </Box>
 
-                <Box sx={{ flexGrow: 0 }}>
-                  <Tooltip title="Open settings">
-                    <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                      <Avatar
-                        alt="Remy Sharp"
-                        src="/static/images/avatar/2.jpg"
-                      />
-                    </IconButton>
-                  </Tooltip>
-                  <Menu
-                    sx={{ mt: "45px" }}
-                    id="menu-appbar"
-                    anchorEl={anchorElUser}
-                    anchorOrigin={{
-                      vertical: "top",
-                      horizontal: "right",
-                    }}
-                    keepMounted
-                    transformOrigin={{
-                      vertical: "top",
-                      horizontal: "right",
-                    }}
-                    open={Boolean(anchorElUser)}
-                    onClose={handleCloseUserMenu}>
-                    {settings.map((setting) => (
-                      <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                        <Typography textAlign="center">{setting}</Typography>
-                      </MenuItem>
-                    ))}
-                  </Menu>
-                </Box>
+                {/* <Box sx={{ flexGrow: 0 }}> */}
+                <div className="appbar-socials">
+                  <a href="https://facebook.com" target="_blank">
+                    <FacebookIcon
+                      fontSize="small"
+                      sx={{ color: "#e21144", marginRight: 2 }}
+                    />
+                  </a>
+                  <a href="https://instagram.com" target="_blank">
+                    <InstagramIcon
+                      fontSize="small"
+                      sx={{ color: "#e21144", marginRight: 2 }}
+                    />
+                  </a>
+                  <a href="https://twitter.com" target="_blank">
+                    <TwitterIcon fontSize="small" sx={{ color: "#e21144" }} />
+                  </a>
+                </div>
+                {/* </Box> */}
               </Toolbar>
             </Container>
           </AppBar>
+          <Container maxWidth="lg" className="carousel-container">
+            <Grid container spacing={2}>
+              <Grid
+                item
+                xs={12}
+                sm={12}
+                md={6}
+                lg={6}
+                xl={6}
+                className="my-intro">
+                <div className="headline">
+                  <div className="title">
+                    Providing creative design and web solutions
+                  </div>
+                  <div className="red-underline"></div>
+                  <div className="author">Frontend Developer</div>
+
+                  <div className="desc">
+                    Hi, I am Winifred. UI/UX designer and web developer. Lorem
+                    ipsum dolor sit amet. Adipiscing consectetur.
+                  </div>
+                  <div className="hero-button-box">
+                    <Button
+                      className="hero-button"
+                      style={{ marginRight: 20 }}
+                      href={resume}
+                      download>
+                      Download CV
+                    </Button>
+                    <Button className="hero-button">Hire Me</Button>
+                  </div>
+                </div>
+              </Grid>
+              <Grid
+                item
+                xs={12}
+                sm={12}
+                md={6}
+                lg={6}
+                xl={6}
+                className="carousel-grid">
+                <div className="carousel-box">
+                  <img
+                    src={blobThree}
+                    alt="Front end development"
+                    className="carousel-img"
+                  />
+                </div>
+              </Grid>
+            </Grid>
+          </Container>
         </Box>
+        <Container maxWidth="xl" className="section-two">
+          <Grid container spacing={2}>
+            <Grid
+              item
+              xs={12}
+              sm={12}
+              md={6}
+              lg={6}
+              xl={6}
+              className="about-container">
+              <div className="about-title">What I do</div>
+              <div className="about">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+                nisl eros, pulvinar facilisis justo mollis, auctor consequat
+                urna. Morbi a bibendum metus. Donec scelerisque sollicitudin.
+              </div>
+              <div className="about">
+                Duis tincidunt laoreet ex, in pretium orci vestibulum eget.
+                Class aptent taciti sociosqu ad litora torquent per conubia
+                nostra, per inceptos himenaeos. Duis pharetra luctus lacus ut
+                vestibulum. Maecenas ipsum lacus.
+              </div>
+            </Grid>
+            <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+              <Portfolio />
+            </Grid>
+          </Grid>
+        </Container>
       </Container>
     </div>
   );
--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,11 +1,145 @@
-import React from "react";
+// import React, { useState } from "react";
+// import PropTypes from "prop-types";
+// import SwipeableViews from "react-swipeable-views";
+// import { useTheme } from "@mui/material/styles";
+// import AppBar from "@mui/material/AppBar";
+// import Tabs from "@mui/material/Tabs";
+// import Tab from "@mui/material/Tab";
+// import Typography from "@mui/material/Typography";
+// import Box from "@mui/material/Box";
+
+// function TabPanel(props) {
+//   const { children, value, index, ...other } = props;
+
+//   return (
+//     <div
+//       role="tabpanel"
+//       hidden={value !== index}
+//       id={`full-width-tabpanel-${index}`}
+//       aria-labelledby={`full-width-tab-${index}`}
+//       {...other}>
+//       {value === index && (
+//         <Box sx={{ p: 3 }}>
+//           <Typography>{children}</Typography>
+//         </Box>
+//       )}
+//     </div>
+//   );
+// }
+
+// TabPanel.propTypes = {
+//   children: PropTypes.node,
+//   index: PropTypes.number.isRequired,
+//   value: PropTypes.number.isRequired,
+// };
+
+// function a11yProps(index) {
+//   return {
+//     id: `full-width-tab-${index}`,
+//     "aria-controls": `full-width-tabpanel-${index}`,
+//   };
+// }
+
+// const Portfolio = () => {
+//   const theme = useTheme();
+//   const [value, setValue] = useState(0);
+
+//   const handleChange = (event, newValue) => {
+//     setValue(newValue);
+//   };
+
+//   const handleChangeIndex = (index) => {
+//     setValue(index);
+//   };
+
+//   return (
+//     <Box sx={{ bgcolor: "background.paper", width: 500 }}>
+//       <AppBar
+//         position="static"
+//         sx={{
+//           bgcolor: "background.paper",
+//           boxShadow: "none",
+//           color: "#ff6347",
+//         }}>
+//         <Tabs
+//           value={value}
+//           onChange={handleChange}
+//           TabIndicatorProps={{ style: { background: "#ff6347" } }}
+//           textColor="inherit"
+//           variant="fullWidth"
+//           aria-label="full width tabs example">
+//           <Tab label="Portfolio" {...a11yProps(0)} />
+//           <Tab label="Resume" {...a11yProps(1)} />
+//         </Tabs>
+//       </AppBar>
+//       <SwipeableViews
+//         axis={theme.direction === "rtl" ? "x-reverse" : "x"}
+//         index={value}
+//         onChangeIndex={handleChangeIndex}>
+//         <TabPanel value={value} index={0} dir={theme.direction}>
+//           Portfolio
+//         </TabPanel>
+//         <TabPanel value={value} index={1} dir={theme.direction}>
+//           Resume
+//         </TabPanel>
+//       </SwipeableViews>
+//     </Box>
+//   );
+// };
+
+// export default Portfolio;
+
+// import React, { useState } from "react";
+// import Box from "@mui/material/Box";
+// import Tab from "@mui/material/Tab";
+// import TabContext from "@mui/lab/TabContext";
+// import TabList from "@mui/lab/TabList";
+// import TabPanel from "@mui/lab/TabPanel";
+
+// const Portfolio = () => {
+//   const [value, setValue] = useState("1");
+
+//   const handleChange = (event, newValue) => {
+//     setValue(newValue);
+//   };
+
+//   return (
+//     <Box sx={{ width: "100%", typography: "body1" }}>
+//       <TabContext value={value}>
+//         <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+//           <TabList onChange={handleChange} aria-label="lab API tabs example">
+//             <Tab label="Portfolio" value="1" />
+//             <Tab label="Resume" value="2" />
+//           </TabList>
+//         </Box>
+//         <TabPanel value="1">Portfolio</TabPanel>
+//         <TabPanel value="2">Resume</TabPanel>
+//       </TabContext>
+//     </Box>
+//   );
+// };
+
+// export default Portfolio;
+
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
 
 const Portfolio = () => {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <div>
-      <h1>portfolio</h1>
-    </div>
+    <Box sx={{ width: "100%", bgcolor: "background.paper" }}>
+      <Tabs value={value} onChange={handleChange} centered>
+        <Tab label="Portfolio" />
+        <Tab label="Resume" />
+      </Tabs>
+    </Box>
   );
 };
-
 export default Portfolio;
